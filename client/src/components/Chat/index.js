f1f904be7ec0d446f2bbe6797cfe88901e48a6f7
Jessica Apolinar <jessica.uk220691@gmail.com>
2020-08-26 00:44:36 -0500

    socket

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import querySring from 'query-string'
+import io from 'socket.io-client'
 
-function Chat() {
+let socket = null;
+const ENDPOINT = 'localhost:5000'
+
+function Chat(props) {
+
+  const {location} = props;
+
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  useEffect(() => {
+    const data = querySring.parse(location.search)
+    
+    socket = io(ENDPOINT)
+
+    console.log(socket);
+    
+
+    setName(data.name)
+    setRoom(data.room)
+
+    socket.emit('join', {name, room}, () => {
+
+    })
+    return () => {
+      socket.emit('disconnect')
+      socket.off()
+    }
+  }, [ENDPOINT, location.search])
   return (
     <div>
       Chat
--- client/src/components/Join/index.js
+++ client/src/components/Join/index.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FormControl, InputLabel, Input, Button } from '@material-ui/core';
+import './join.css'
+
 
 function Join() {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  const handleChangeName = (e) => {
+    setName(e.target.value)
+  }
+  const handleChangeRoom = (e) => {
+    setRoom(e.target.value)
+  }
+  const hadleSubmit = (e) => {
+    e.preventDefault()
+  }
+  const handleClickLink = (e) => {
+    if (name === '' || room === '') {
+      e.preventDefault()
+    }
+  }
   return (
-    <div>
-      Join
+    <div className="join__outerContainer">
+      <div className="join__innerContainer">
+        <h1 className="join__header">Join</h1>
+        <FormControl>
+          <InputLabel htmlFor="input-name"></InputLabel>
+          <Input className="join__input" onChange={handleChangeName} />
+          <InputLabel htmlFor="input-room"></InputLabel>
+          <Input className="join__input" onChange={handleChangeRoom} />
+          <Link onClick={handleClickLink} to={`/chat?name=${name}&room=${room}`}>
+            <Button className="join__button" variant="contained" color="primary">Submit</Button>
+          </Link>
+        </FormControl>
+      </div>
     </div>
   )
 }
